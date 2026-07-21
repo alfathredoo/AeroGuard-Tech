f5fc569a3292482d9e2541f4d58a6fa1d37be7b5
alfathredoo <117902182+alfathredoo@users.noreply.github.com>
2026-07-21 19:34:39 +0700

    Create registration form in register.php
    Add basic HTML structure for registration form.

--- register.php
+++ register.php
@@ -1,1 +1,14 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>R</title>
+</head>
+<body>
+    <form action="register">
+        <input type="text" id="username" name="username" required>
+        <input type="text" id="password" name="password">
+    </form>
+</body>
+</html>
